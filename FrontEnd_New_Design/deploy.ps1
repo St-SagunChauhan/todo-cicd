67551d1dfe011ae1af94322d495aefd01d3928a1
Sagun <st.sagunchauhan1@gmail.com>
2024-01-29 17:01:02 +0530

    Modified yaml

--- FrontEnd_New_Design/deploy.ps1
+++
@@ -1,4 +0,0 @@
-$SourcePath = "build/*"  # Path to your React build folder
-$DestinationPath = "C:/inetpub/crm-test"  # Path to your IIS site directory
-
-Copy-Item -Path $SourcePath -Destination $DestinationPath -Recurse -Force
